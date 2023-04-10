1e51c29871587d7192d09c296c5a7da2024e3d3f
Bibi Asha <bibiashakadmeshwar@gmail.com>
2023-04-10 12:58:07 +0530

    Updates

---
+++ precache-manifest.9482b42c85716874c852da93d52829cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e72cb19604c541b777f7",
+    "url": "/CRUD_react.js/static/css/main.97c2eb74.chunk.css"
+  },
+  {
+    "revision": "e72cb19604c541b777f7",
+    "url": "/CRUD_react.js/static/js/main.e72cb196.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/CRUD_react.js/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "d25e0f86c872b800dbc6",
+    "url": "/CRUD_react.js/static/js/2.d25e0f86.chunk.js"
+  },
+  {
+    "revision": "c59771762529296274f5",
+    "url": "/CRUD_react.js/static/js/runtime~main.c5977176.js"
+  },
+  {
+    "revision": "4eb9bb4004996999c7bac8e470a33356",
+    "url": "/CRUD_react.js/index.html"
+  }
+];
